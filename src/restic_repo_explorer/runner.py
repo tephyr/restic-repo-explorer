5c49c2694b3ce21b4b16bb063477335c0beeebc0
Andrew Ittner <aji@rhymingpanda.com>
2024-09-18 15:17:56 -0700

    Add bindings for footer, open settings screen

--- src/restic_repo_explorer/runner.py
+++ src/restic_repo_explorer/runner.py
@@ -3,28 +3,33 @@ from textual.widgets import Static, Input, Button, Footer
 from textual.css.query import NoMatches
 from textual.containers import Vertical
 from .settings import SettingsModal
-from textual.binding import BindingType
+from textual.binding import Binding, BindingType
 
 class ThreePaneApp(App):
     CSS_PATH = "styles.tcss"
-    def compose(self) -> ComposeResult:
+    BINDINGS = [
+        Binding(key="q", action="quit", description="Quit the app"),
+        Binding(key="t", action="settings", description="Settings")
+    ]
+
+    def compose(self) -> ComposeResult:        
         with Vertical(classes="pane top-pane"):
             yield Static("Repository Configuration", id="title")
-            yield Input(placeholder="Repository", id="repository")
-            yield Input(placeholder="Password file", id="password_file")
-            yield Input(placeholder="Additional field", id="additional_field")
-            yield Button("OK", id="ok_button")
+            # yield Input(placeholder="Repository", id="repository")
+            # yield Input(placeholder="Password file", id="password_file")
+            # yield Input(placeholder="Additional field", id="additional_field")
+            # yield Button("OK", id="ok_button")
         yield Static("Pane 2", classes="pane")
         yield Static("Pane 3", classes="pane")
         yield Footer()
-
-    def on_mount(self) -> None:
-        self.query_one(Footer).add_button("Settings", "settings", "Show Settings")
 
     def on_footer_button_pressed(self, event: BindingType) -> None:
         if event.button.id == "settings":
             self.push_screen(SettingsModal())
         yield Footer()
+
+    def action_settings(self):
+        self.push_screen(SettingsModal())
 
 def run_app():
     app = ThreePaneApp()
